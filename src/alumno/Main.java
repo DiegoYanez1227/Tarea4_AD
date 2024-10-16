c3edfb521a904fca40dc47e6565a2851547781e5
DiegoYanez1227 <diegoyan0260@gmail.com>
2024-10-16 21:02:07 +0200

    Update Main.java

--- src/alumno/Main.java
+++ src/alumno/Main.java
@@ -1,5 +1,10 @@
 package alumno;
 
+/**
+ * Clase Main que gestiona la ejecucion del programa
+ * 
+ * @author Diego
+ */
 public class Main {
 	public static void main(String[] args) {
 		GestorAlumno g = new GestorAlumno();
